936f3bbe69fa72aeae7d62b2992b0a2ba3c86a2e
csc5090 <tkdcjf5090@gmail.com>
2025-11-13 19:44:55 +0900

    123123

--- dummy1/src/MessageService.java
+++ dummy1/src/MessageService.java
@@ -1,1 +1,10 @@
-"simple Message"
+package kr.or.ddit.service;
+import net.datafaker.Faker;
+
+public class MessageService{
+	public String getMessage() {
+		Faker faker = new Faker();
+		return faker.name().fullName();
+	}
+	
+}
--- dummy1/src/PrintMessage.java
+++ dummy1/src/PrintMessage.java
@@ -1,9 +1,14 @@
 package kr.or.ddit;
-
+import kr.or.ddit.service.MessageService;
+import java.util.*;
 
 public class PrintMessage {
 	public static void main(String[] args) {
-		String message = ;
-		System.out.println(message);
+		MessageService service = 
+		new MessageService();
+		String message1 = service.getMessage();
+		String message2 = service.getMessage();
+		List<String> messages = List.of(message1, message2);
+		System.out.println(messages);
 	}
 }
